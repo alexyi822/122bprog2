89c9ad4738b94936bacdd4ffb684a5a3b2e99f59
stcalica <kycalica@gmail.com>
2016-06-02 21:23:04 -0700

    started dselect

--- detselect.py
+++ detselect.py
@@ -2,8 +2,27 @@
 
 import sys
 
-def deterministic_select(arr):
-  
+
+def deterministic_select(L, k):
+    pass
+    if(L < 10):
+        #sort L
+        return L[k]
+    for( i in (len(L)/5)):
+        x[i] = select(L[i], 3)
+
+    M = select(L[i], (len(n)/10))
+
+    left = [x for x in L[i] if L[i] < M]
+    right = [x for x in L[i] if L[i] > M]
+    center = [x for x in L[i] if L[i] == M]
+
+    if(k <= len(left)):
+            return select(left,k)
+    if(k > len(left)+len(center)):
+            return(right, k-len(left)-len(center))
+    else:
+            return M
 
 def main():
   k = int(sys.argv[2])
@@ -12,7 +31,7 @@ def main():
     for line in f:
       nums.append(int(line))
 
-  
+
 
 
 if __name__ == "__main__":
